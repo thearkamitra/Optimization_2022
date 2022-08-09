e0703483fc45c4df9f0d8726e14ca3a8e13ac01a
Arka Mitra <amitra@ethz.ch>
2022-08-09 15:53:04 +0200

    PSO range added

--- PSO.py
+++ PSO.py
@@ -43,6 +43,18 @@ def _parse_args():
         default=0,
         help="Minimum value of the elements initially",
     )
+    parser.add_argument(
+        "--max_range",
+        type=float,
+        default=np.inf,
+        help="Maximum range of the elements initially",
+    )
+    parser.add_argument(
+        "--min_range",
+        type=float,
+        default=-np.inf,
+        help="Minimum range of the elements initially",
+    )
     parser.add_argument("--c1", type=float, default=2, help="Positional Dependence")
     parser.add_argument("--c2", type=float, default=2, help="Global Dependence")
     parser.add_argument("--modified", action="store_true", help="Use the modified PSO")
@@ -52,7 +64,9 @@ def _parse_args():
         default=1000,
         help="Number of iterations the algorithm will run",
     )
-    parser.add_argument("--probability", type=float, default=0.001, help="Probability of new generation")
+    parser.add_argument(
+        "--probability", type=float, default=0.001, help="Probability of new generation"
+    )
     parser.add_argument(
         "--func",
         type=str,
@@ -79,8 +93,10 @@ def _parse_args():
     )
     return parser.parse_args()
 
-def get_distance(x,y):
-    return np.max(np.sum((x-y)**2,axis=1))**0.5
+
+def get_distance(x, y):
+    return np.max(np.sum((x - y) ** 2, axis=1)) ** 0.5
+
 
 def main():
     # initialisation
@@ -89,38 +105,53 @@ def main():
     num_feat = args.num_feat
     if args.func == "ackley":
         function = func.ackley
+        args.max_range, args.min_range = (32, -32)
     elif args.func == "beale":
         function = func.beale
+        args.max_range, args.min_range = (4.5, -4.5)
     elif args.func == "booth":
         function = func.booth
+        args.max_range, args.min_range = (10, -10)
     elif args.func == "bukin6":
         function = func.bukin6
+        args.max_range, args.min_range = (3, -3)
     elif args.func == "crossintray":
         function = func.crossintray
+        args.max_range, args.min_range = (10, -10)
     elif args.func == "easom":
         function = func.easom
+        args.max_range, args.min_range = (100, -100)
     elif args.func == "eggholder":
         function = func.eggholder
+        args.max_range, args.min_range = (512, -512)
     elif args.func == "goldstein":
         function = func.goldstein
+        args.max_range, args.min_range = (2, -2)
     elif args.func == "himmelblau":
         function = func.himmelblau
+        args.max_range, args.min_range = (5, -5)
     elif args.func == "holdertable":
         function = func.holdertable
+        args.max_range, args.min_range = (10, -10)
     elif args.func == "levi":
         function = func.levi
+        args.max_range, args.min_range = (10, -10)
     elif args.func == "matyas":
         function = func.matyas
+        args.max_range, args.min_range = (10, -10)
     elif args.func == "rastringin":
         function = func.rastrigin
+        args.max_range, args.min_range = (5.12, -5.12)
     elif args.func == "rosenbrock":
         function = func.rosenbrock
     elif args.func == "schaffer2":
         function = func.schaffer2
+        args.max_range, args.min_range = (100, -100)
     elif args.func == "sphere":
         function = func.sphere
     elif args.func == "threehump":
         function = func.threehump
+        args.max_range, args.min_range = (5, -5)
     else:
         function = f
     position = np.random.random((num_units, num_feat))
@@ -141,9 +172,14 @@ def main():
             - args.c2 * r2 * (position - best_pos)
         )
         if args.modified:
-            prob = np.random.random((args.num_units,1))< args.probability
-            position = position*(1-prob) + prob*(best_pos+np.random.randn(*best_pos.shape)*get_distance(best_pos, position)/100)
-            velocity = velocity*(1-prob) 
+            prob = np.random.random((args.num_units, 1)) < args.probability
+            position = position * (1 - prob) + prob * (
+                best_pos
+                + np.random.randn(*best_pos.shape)
+                * get_distance(best_pos, position)
+                / 100
+            )
+            velocity = velocity * (1 - prob)
         position = position + velocity
         valuesnow = function(position)
         temp = valuesnow < indibestnow
